b0795df7cda2a400b7acb68902847fda1a242646
june4m <congminh04062004@gmail.com>
2025-11-25 11:28:44 +0700

    change constructure folder

---
+++ CloudRacketSystem/BE/services/auth-service/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import router from './routers/users.routers';
+const app = express();
+const port = 3000;
+
+app.get('/api', router);
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
+app.use(express.json());
+app.use(cookieParser());
