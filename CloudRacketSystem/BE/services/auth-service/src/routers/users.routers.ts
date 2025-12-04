5b201258dc2fa342be279f0f1d31a5c4754b9685
june4m <congminh04062004@gmail.com>
2025-12-04 21:52:19 +0700

    update sth

--- CloudRacketSystem/BE/services/auth-service/src/routers/users.routers.ts
+++ CloudRacketSystem/BE/services/auth-service/src/routers/users.routers.ts
@@ -8,5 +8,6 @@ const courtController = new CourtController()
 // GET /getCourt
 console.log('Mount /getCourt route')
 router.get('/getCourt', (req, res) => courtController.getCourt(req, res))
+// router.get('/login',(req,res)=>userController.login(req,res))
 
 export default router
